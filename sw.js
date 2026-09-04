144bc3e1c9190f12283230c405413ddd40d46335
kanotyo <springfield4743@gmail.com>
2026-09-04 16:33:31 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 "use strict";
-const CACHE="hoshiakari-v1.2.7";
+const CACHE="hoshiakari-v1.2.8";
 const ASSETS=["./","./index.html","./style.css","./game.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
